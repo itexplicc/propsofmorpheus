95db218eb3c52c83ad58c7c43eeaf90333faa78d
itexplicc <it.explicceylon@gmail.com>
2026-07-25 18:24:53 +0530

    Add Hi-Line Auto configuration

---
+++ hi-line-auto/config.js
@@ -0,0 +1,6 @@
+window.HILINE_CONFIG = {
+  supabaseUrl: "https://zadxvmpgngwtpsmdkcod.supabase.co",
+  publishableKey: "sb_publishable_MxL_uLP-7iWrPDIM2J_RPQ_cDiVHZoA",
+  apiUrl: "https://zadxvmpgngwtpsmdkcod.supabase.co/functions/v1/hiline-api",
+  publicBaseUrl: "https://itexplicc.github.io/propsofmorpheus/hi-line-auto/"
+};
